0b2331c886e431f7fc2e55dc0cf02697671c98b2
Joris1225 <jorisvanvugt@live.nl>
2014-12-06 17:59:26 +0100

    Added sky and fixed fog

--- LD31/game.ts
+++ LD31/game.ts
@@ -1,4 +1,5 @@
 ﻿/// <reference path="scripts/typings/threejs/three.d.ts" />
+/// <reference path="input.ts" />
 
 class Game {
 
@@ -10,8 +11,11 @@ class Game {
     foliage: THREE.Mesh[];
     treeOffset: number = 1000;
     moveSpeed: number = 16;
+    input: Input;
+    skyLine: THREE.Mesh;
 
     constructor(content: HTMLElement) {
+        this.input = new Input();
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(960, 600);
         this.camera = new THREE.PerspectiveCamera(60, 16 / 9, 0.1, 10000);
@@ -31,6 +35,15 @@ class Game {
     }
 
     update() {
+
+        if (68 in this.input.keysDown && this.camera.position.x < 150) {
+            this.camera.position.x += 5;
+        }
+
+        if (65 in this.input.keysDown && this.camera.position.x > -150) {
+            this.camera.position.x -= 5;
+        }
+
         this.trees.forEach((tree: THREE.Mesh) => {
             if (tree.position.z >= this.camera.position.z) {
                 tree.translateZ(-this.treeOffset * this.trees.length / 2);
@@ -48,6 +61,7 @@ class Game {
                 fol.translateZ(this.moveSpeed);
             }
         });
+        this.skyLine.position.x = this.camera.position.x;
     }
 
     render() {
@@ -60,14 +74,14 @@ class Game {
         this.scene.add(this.camera);
 
         this.road = this.loadPlane(100, 100, "road.png");
-        this.road.translateY(-150);
+        this.road.translateY(-100);
         this.road.translateZ(500);
         this.road.rotateX(-Math.PI / 2);
         this.road.scale.x = 120;
         this.road.scale.y = 1000;
 
-        var tree: THREE.Mesh = this.loadPlane(100, 100, "tree.png");
-        tree.translateY(-125);
+        var tree: THREE.Mesh = this.loadPlane(100, 200, "tree.png");
+        tree.translateY(-75);
         tree.scale.y = 4;
         tree.translateZ(-1000);
         this.trees = [];
@@ -78,14 +92,15 @@ class Game {
             this.trees.push(newTree);
         }
 
-        var skyline: THREE.Mesh = this.loadPlane(400, 100, "skyline.png");
-        skyline.translateZ(-6000);
-        skyline.scale.x = 140;
-        skyline.scale.y = 30;
-        skyline.translateY(30 * 100 / 2 - 300);
+        this.skyLine = this.loadPlane(400, 100, "skyline.png");
+        this.skyLine.material.setValues({ fog: false });
+        this.skyLine.translateZ(-6000);
+        this.skyLine.scale.x = 140;
+        this.skyLine.scale.y = 30;
+        this.skyLine.translateY(30 * 100 / 2 - 300);
 
         var grass: THREE.Mesh = this.loadPlane(100, 100, "foliage.png");
-        grass.translateY(-100);
+        grass.translateY(-50);
         grass.scale.y = 0.5;
         grass.translateZ(-1000);
         this.foliage = [];
@@ -98,7 +113,15 @@ class Game {
         }
         this.foliage.forEach((fol: THREE.Mesh) => this.scene.add(fol));
 
-        this.scene.add(skyline);
+        var sky: THREE.Mesh = this.loadPlane(100, 100, "sky.png");
+        sky.material.setValues({ fog: true });
+        sky.translateY(2000);
+        sky.translateZ(this.skyLine.position.z - 50);
+        sky.scale.x = 140;
+        sky.scale.y = 60;
+        this.scene.add(sky);
+
+        this.scene.add(this.skyLine);
         this.trees.forEach((tree: THREE.Mesh) => this.scene.add(tree));
         this.scene.add(this.road);
     }
--- LD31/ld31.js
+++ LD31/ld31.js
@@ -1,4 +1,22 @@
-﻿/// <reference path="scripts/typings/threejs/three.d.ts" />
+﻿/// <reference path="game.ts" />
+var Input = (function () {
+    function Input() {
+        var _this = this;
+        this.keysDown = {};
+        addEventListener("keydown", function (e) {
+            _this.keysDown[e.keyCode] = true;
+            if (e.keyCode == 32 || e.keyCode == 16) {
+                e.preventDefault();
+            }
+        });
+        addEventListener("keyup", function (e) {
+            delete _this.keysDown[e.keyCode];
+        });
+    }
+    return Input;
+})();
+/// <reference path="scripts/typings/threejs/three.d.ts" />
+/// <reference path="input.ts" />
 var Game = (function () {
     function Game(content) {
         var _this = this;
@@ -9,6 +27,7 @@ var Game = (function () {
             _this.render();
             window.requestAnimationFrame(_this.tick);
         };
+        this.input = new Input();
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(960, 600);
         this.camera = new THREE.PerspectiveCamera(60, 16 / 9, 0.1, 10000);
@@ -22,6 +41,14 @@ var Game = (function () {
 
     Game.prototype.update = function () {
         var _this = this;
+        if (68 in this.input.keysDown && this.camera.position.x < 150) {
+            this.camera.position.x += 5;
+        }
+
+        if (65 in this.input.keysDown && this.camera.position.x > -150) {
+            this.camera.position.x -= 5;
+        }
+
         this.trees.forEach(function (tree) {
             if (tree.position.z >= _this.camera.position.z) {
                 tree.translateZ(-_this.treeOffset * _this.trees.length / 2);
@@ -37,6 +64,7 @@ var Game = (function () {
                 fol.translateZ(_this.moveSpeed);
             }
         });
+        this.skyLine.position.x = this.camera.position.x;
     };
 
     Game.prototype.render = function () {
@@ -50,14 +78,14 @@ var Game = (function () {
         this.scene.add(this.camera);
 
         this.road = this.loadPlane(100, 100, "road.png");
-        this.road.translateY(-150);
+        this.road.translateY(-100);
         this.road.translateZ(500);
         this.road.rotateX(-Math.PI / 2);
         this.road.scale.x = 120;
         this.road.scale.y = 1000;
 
-        var tree = this.loadPlane(100, 100, "tree.png");
-        tree.translateY(-125);
+        var tree = this.loadPlane(100, 200, "tree.png");
+        tree.translateY(-75);
         tree.scale.y = 4;
         tree.translateZ(-1000);
         this.trees = [];
@@ -68,14 +96,15 @@ var Game = (function () {
             this.trees.push(newTree);
         }
 
-        var skyline = this.loadPlane(400, 100, "skyline.png");
-        skyline.translateZ(-6000);
-        skyline.scale.x = 140;
-        skyline.scale.y = 30;
-        skyline.translateY(30 * 100 / 2 - 300);
+        this.skyLine = this.loadPlane(400, 100, "skyline.png");
+        this.skyLine.material.setValues({ fog: false });
+        this.skyLine.translateZ(-6000);
+        this.skyLine.scale.x = 140;
+        this.skyLine.scale.y = 30;
+        this.skyLine.translateY(30 * 100 / 2 - 300);
 
         var grass = this.loadPlane(100, 100, "foliage.png");
-        grass.translateY(-100);
+        grass.translateY(-50);
         grass.scale.y = 0.5;
         grass.translateZ(-1000);
         this.foliage = [];
@@ -90,7 +119,15 @@ var Game = (function () {
             return _this.scene.add(fol);
         });
 
-        this.scene.add(skyline);
+        var sky = this.loadPlane(100, 100, "sky.png");
+        sky.material.setValues({ fog: true });
+        sky.translateY(2000);
+        sky.translateZ(this.skyLine.position.z - 50);
+        sky.scale.x = 140;
+        sky.scale.y = 60;
+        this.scene.add(sky);
+
+        this.scene.add(this.skyLine);
         this.trees.forEach(function (tree) {
             return _this.scene.add(tree);
         });
