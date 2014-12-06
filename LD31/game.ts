56bf38e44f8227e620b3b5af3c011deb7e14e607
Joris1225 <jorisvanvugt@live.nl>
2014-12-06 14:41:19 +0100

    Added trees

--- LD31/game.ts
+++ LD31/game.ts
@@ -6,6 +6,8 @@ class Game {
     camera: THREE.Camera;
     scene: THREE.Scene;
     road: THREE.Mesh;
+    trees: THREE.Mesh[];
+    treeOffset: number = 1000;
 
     constructor(content: HTMLElement) {
         this.renderer = new THREE.WebGLRenderer();
@@ -14,12 +16,27 @@ class Game {
         this.camera.position.z = 300;
         this.scene = new THREE.Scene();
         this.scene.add(this.camera);
+
         this.road = this.loadPlane(100, 100, "road.png");
         this.road.translateY(-150);
         this.road.translateZ(500);
         this.road.rotateX(-Math.PI / 2);
         this.road.scale.x = 15;
         this.road.scale.y = 1000;
+
+        var tree: THREE.Mesh = this.loadPlane(100, 100, "tree.png");
+        tree.translateY(-100);
+        tree.scale.y = 4;
+        tree.translateZ(-1000);
+        this.trees = [];
+        for (var i = 0; i < 5 * 2; i++) {
+            var newTree: THREE.Mesh = tree.clone();
+            newTree.translateX(350 * ((i % 2 == 0) ? -1 : 1));
+            newTree.translateZ(Math.floor(i / 2) * this.treeOffset);
+            this.trees.push(newTree);
+        }
+
+        this.trees.forEach((tree: THREE.Mesh) => this.scene.add(tree));
         this.scene.add(this.road);
         content.appendChild(this.renderer.domElement);
     }
@@ -35,7 +52,14 @@ class Game {
     }
 
     update() {
-        
+        this.trees.forEach((tree: THREE.Mesh) => {
+            if (tree.position.z >= this.camera.position.z) {
+                tree.translateZ(-this.treeOffset * this.trees.length / 2);
+            }
+            else {
+                tree.translateZ(16)
+            }
+        });
     }
 
     render() {
@@ -46,7 +70,7 @@ class Game {
         var texture: THREE.Texture = THREE.ImageUtils.loadTexture(textureURL);
         texture.magFilter = THREE.NearestFilter;
         texture.minFilter = THREE.NearestFilter;
-        return new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({map: texture}));
+        return new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({ map: texture, transparent: true}));
     }
 }
 
--- LD31/ld31.js
+++ LD31/ld31.js
@@ -2,6 +2,7 @@
 var Game = (function () {
     function Game(content) {
         var _this = this;
+        this.treeOffset = 1000;
         this.tick = function () {
             _this.update();
             _this.render();
@@ -13,12 +14,29 @@ var Game = (function () {
         this.camera.position.z = 300;
         this.scene = new THREE.Scene();
         this.scene.add(this.camera);
+
         this.road = this.loadPlane(100, 100, "road.png");
         this.road.translateY(-150);
         this.road.translateZ(500);
         this.road.rotateX(-Math.PI / 2);
         this.road.scale.x = 15;
         this.road.scale.y = 1000;
+
+        var tree = this.loadPlane(100, 100, "tree.png");
+        tree.translateY(-100);
+        tree.scale.y = 4;
+        tree.translateZ(-1000);
+        this.trees = [];
+        for (var i = 0; i < 5 * 2; i++) {
+            var newTree = tree.clone();
+            newTree.translateX(350 * ((i % 2 == 0) ? -1 : 1));
+            newTree.translateZ(Math.floor(i / 2) * this.treeOffset);
+            this.trees.push(newTree);
+        }
+
+        this.trees.forEach(function (tree) {
+            return _this.scene.add(tree);
+        });
         this.scene.add(this.road);
         content.appendChild(this.renderer.domElement);
     }
@@ -27,6 +45,14 @@ var Game = (function () {
     };
 
     Game.prototype.update = function () {
+        var _this = this;
+        this.trees.forEach(function (tree) {
+            if (tree.position.z >= _this.camera.position.z) {
+                tree.translateZ(-_this.treeOffset * _this.trees.length / 2);
+            } else {
+                tree.translateZ(16);
+            }
+        });
     };
 
     Game.prototype.render = function () {
@@ -37,7 +63,7 @@ var Game = (function () {
         var texture = THREE.ImageUtils.loadTexture(textureURL);
         texture.magFilter = THREE.NearestFilter;
         texture.minFilter = THREE.NearestFilter;
-        return new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({ map: texture }));
+        return new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({ map: texture, transparent: true }));
     };
     return Game;
 })();
