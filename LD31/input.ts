d82c4558c15c34c502cf25c55b7daef805f02732
Joris1225 <jorisvanvugt@live.nl>
2014-12-06 17:59:16 +0100

    Added Input class

---
+++ LD31/input.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="game.ts" />
+
+class Input {
+    keysDown: { [id: number]: boolean; } = {};
+
+    constructor() {
+        addEventListener("keydown", (e) => {
+            this.keysDown[e.keyCode] = true;
+            if (e.keyCode == 32 || e.keyCode == 16) {
+                e.preventDefault();
+            }
+        });
+        addEventListener("keyup", (e) => {
+            delete this.keysDown[e.keyCode];
+        });
+    }
+}
